b399cafce311c41ae9c3cc9e052dbe140912e3b6
Meraj Meraj <devmeraz87@gmail.com>
2023-06-19 12:34:16 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-// import v from "./1.mp4"
+import { useEffect, useRef } from "react";
+
+
+import v from "./1.mp4"
 import _a from "./1.mp3"
 
 function App() {
 
+ 
+
   return (
     <div className="App">
-      {/* <video src={v} autoPlay loop playsInline></video> */}
-      <audio src={_a} autoPlay loop playsInline></audio>
+
+      <video autoPlay={true} playsInline src={v}></video>
+
     </div>
   );
 }
